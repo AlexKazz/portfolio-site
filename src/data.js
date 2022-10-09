ec980127e022a1c87ad4bd0b2ef18addab265473
AlexKazz <akazenoff@gmail.com>
2022-10-08 17:24:02 -0700

    add Project component content and 2 projects

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    title: 'Scribble Me This',
+    subtitle:
+      'TensorFlow.js, ML5, Javascript, React, Redux, Express, Socket.io, Heroku',
+    description:
+      'Pictionary-style game in which players compete to have their drawings recognized by an AI',
+    image: './scribble.png',
+  },
+  {
+    title: 'Grace Shopper (E-Commerce Site)',
+    subtitle:
+      'Javascript, React, Redux, Bcrypt/JWT, Express, PostgreSQL, Sequelize',
+    description:
+      'E-commerce app with user & cart functionality, using REST API',
+    image: './grace-shopper.png',
+  },
+];
