2b2c859f442419879122ac477c95b5b86199b1e2
AlexKazz <akazenoff@gmail.com>
2022-10-10 12:26:33 -0700

    add contact email functionality, minor syntax edit

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -12,7 +12,7 @@ export default function Blog() {
         </h1>
         <div className='flex flex-wrap m-4'>
           {blogPosts.map((blogPost) => (
-            <div className='p-4 md:w-1/2 w-full'>
+            <div key={blogPost.content} className='p-4 md:w-1/2 w-full'>
               <div className='h-full bg-gray-800 bg-opacity-40 p-8 rounded'>
                 <CommandLineIcon className='block w-8 text-gray-500 mb-4' />
                 <p className='leading-relaxed mb-6'>{blogPost.content}</p>
