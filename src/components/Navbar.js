e1fbdd88a1e8dc24110ca7200425f231f2b07b0d
AlexKazz <akazenoff@gmail.com>
2022-10-10 15:23:31 -0700

    fix navbar centering in phone display

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,18 +8,18 @@ export default function Navbar() {
     <header className='bg-gray-800 md:sticky top-0 z-10'>
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
         <h2 className='title-font font-medium text-white mb-4 md:mb-0'>
-          <a href='#about' className='ml-3 text-xl'>
+          <a href='#about' className='mx-2 text-xl'>
             Alex Kazenoff
           </a>
         </h2>
         <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center'>
-          <a href='#projects' className='mr-5 hover:text-white'>
+          <a href='#projects' className='mx-2 hover:text-white'>
             Skills
           </a>
-          <a href='#blog' className='mr-5 hover:text-white'>
+          <a href='#blog' className='mx-2 hover:text-white'>
             Blog
           </a>
-          <a href='#contact' className='mr-5 hover:text-white'>
+          <a href='#contact' className='mx-2 hover:text-white'>
             Contact
           </a>
         </nav>
