e4ba5396f90ab03a303f7ae81e77430b2a797cff
AlexKazz <akazenoff@gmail.com>
2023-01-05 13:57:53 -0800

    update navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
-import Blog from './components/Blog';
+import Posts from './components/Posts';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<About />}></Route>
         <Route path='/projects' element={<Projects />}></Route>
+        <Route path='/posts' element={<Posts />}></Route>
         <Route path='/contact' element={<Contact />}></Route>
       </Routes>
 
